f18228b4a632220e6b0728529d8dd0ca48018d92
Bongani Mbigi <bongani@transmute.industries>
2019-02-24 20:38:33 -0600

    added user trigger to create user in firestore

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const firestore = admin.firestore();
+
+exports.createUserDoc = functions.auth.user().onCreate((user) => {
+    
+    const userRef = firestore.doc(`users/${user.uid}`);
+
+    userRef.set({
+        email: user.email,
+        roles: [],
+    });
+});
